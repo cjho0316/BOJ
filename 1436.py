123d6b37101e1c8d2d1a0ba988ef3963cff4784d
cjho0316 <83046766+cjho0316@users.noreply.github.com>
2022-07-19 23:38:04 +0900

    브루트 포스_영화감독 숌

---
+++ 1436.py
@@ -0,0 +1,13 @@
+# 1436
+
+# input_init
+A = int(input())
+num, cnt = 666, 0
+
+while (True):
+    if '666' in str(num):
+        cnt += 1
+        if cnt == A:
+            break
+    num+=1
+print(num)
