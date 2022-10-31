04dc726735b834f6ae8089c1ce7e6bf225c1a964
cjho0316 <83046766+cjho0316@users.noreply.github.com>
2022-10-31 16:31:55 +0900

    기타_팰린드롬수

---
+++ 1259.py
@@ -0,0 +1,19 @@
+#1259
+
+while True:
+    i = 0
+    flag = 0
+    K = str(input())
+    if K == '0': break
+    A = list(K)
+    N = len(A) - 1
+    while i < N:
+        if A[i] != A[N]:
+            flag = 1
+            break
+        i += 1
+        N -= 1
+    if flag == 1:
+        print('no')
+    elif flag == 0:
+        print('yes')
