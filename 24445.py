930a572ea2c88da592827c10c0ba69f1fabee965
cjho0316 <83046766+cjho0316@users.noreply.github.com>
2022-11-07 18:04:27 +0900

    그래프와 순회_알고리즘 수업 - 너비 우선 탐색 2

---
+++ 24445.py
@@ -0,0 +1,29 @@
+#24445
+
+from collections import deque
+
+def bfs(graph, v, visited):
+    cnt = 1
+    queue = deque([v])
+    visited[v] = cnt
+    while queue:
+        v = queue.popleft()
+        for i in graph[v]:
+            if not visited[i]:
+                cnt += 1
+                visited[i] = cnt
+                queue.append(i)
+
+N, M, R = map(int, input().split())
+K = [[] for _ in range(N+1)]
+visited = [0 for _ in range(N+1)]
+for i in range(M):
+    x, y = map(int, input().split())
+    K[x].append(y)
+    K[y].append(x)
+
+for j in range(N+1):
+    K[j].sort(reverse=True)
+
+bfs(K, R, visited)
+print(*visited[1:])
