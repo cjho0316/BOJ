90b2ca9e0c062e486b609f331942147e33b611a9
cjho0316 <83046766+cjho0316@users.noreply.github.com>
2023-01-31 10:39:42 +0900

    동적 계획법 1_연속합

--- 1912.py
+++ 1912.py
@@ -4,5 +4,13 @@ S = list(map(int, input().split()))
 
 for i in range(1, N):
     S[i] = max(S[i], S[i-1] + S[i])
-
 print(max(S))
+
+
+# N = int(input())
+# S = list(map(int, input().split()))
+
+# for i in range(1, N):
+#     S[i] = max(S[i], S[i-1] + S[i])
+
+# print(max(S))
