b60b7782d7dad5db2d4ff23bbf55ad0d6753f3b0
cjho0316 <83046766+cjho0316@users.noreply.github.com>
2023-03-19 23:57:36 +0900

    삼성 SW 역량 테스트 기출 문제_시험 감독

--- 13458.py
+++ 13458.py
@@ -1,12 +1,13 @@
 #13458
 import math
 
-cnt = 0
 N = int(input())
 A = list(map(int, input().split()))
 B, C = map(int, input().split())
 
-for i in A:
-    cnt += math.ceil((i - B) / C)
-
+cnt = 0
+for a in A:
+    if a > B:
+        cnt += math.ceil((a - B) / C)
+        
 print(cnt + N)
